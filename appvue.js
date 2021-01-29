826739be5e9a97abc10e02f9aa6d7c0d472a91a4
unknown <dielasu@gmail.com>
2021-01-29 01:00:10 +0100

    finished input error left to fix

--- appvue.js
+++ appvue.js
@@ -1,20 +1,33 @@
 const app = Vue.createApp({
     data() {
         return {
-            inputNumber: "123456",
-            inputNumberTwo: []
-         
+            inputNumber: "",
         }
     },
     methods: {
         clearAll(){
-            this.inputNumber= "" //Nos vacía el Array
+            this.inputNumber= "" 
         },
-        // f() que ens concatenara el valor del numero clickat amb el que ja existeixi al inputNumber (linea4)
-        addNumber(n){
+        // f() que ens concatenaran els valors dels numeros o els simbols clickats amb el que ja existeixi 
+        // dins de  inputNumber (linea4)
+        numbersChoice(n){
             this.inputNumber = `${this.inputNumber}${n}`
-        }
+        },
+        plusButton(n){
+            this.inputNumber = `${this.inputNumber}${n}`
+        },
+        reduceButton(n){
+            this.inputNumber = `${this.inputNumber}${n}`
+        },
+        multButton(n){
+            this.inputNumber = `${this.inputNumber}${n}`
+        },
+        divButton(n){
+            this.inputNumber = `${this.inputNumber}${n}`
+        },          
+        equal(){
+            this.inputNumber = eval(this.inputNumber)               
+        },
     }
 })
-
 app.mount("#app") 
